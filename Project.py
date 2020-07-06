0b1fca65fdf46eecbfeefd7408bdfc867a06e8ec
shabarish1403 <shabarish.14b@gmail.com>
2020-07-07 03:12:18 +0530

    Reading the CSV file and writing into text file

--- Project.py
+++ Project.py
@@ -1,1 +1,24 @@
+import csv
 
+data = csv.reader(open('/home/shabarish/Desktop/data.csv'))
+table = list(data)
+print(table)
+
+l = len(table)
+table1 = table[0]
+i=1
+while i<l:
+    table1+=table[i]
+    i+=1
+print(table1)
+
+if len(table1) == len(set(table1)):
+    print('No Duplicates found')
+else:
+    print('Duplicates found')
+
+table2 = set(table1)
+print(table2)
+
+with open('/home/shabarish/Desktop/text.txt','w') as f:
+    f.write('\n'.join(table2))
